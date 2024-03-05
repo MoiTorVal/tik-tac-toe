edfd5895c612d6d26e3d5f1a8a227bb67920b43f
moises valdez <moisesvaldez0559@gmail.com>
2024-03-05 13:38:44 -0800

    finsihed tic-tac-toe

--- src/Game.jsx
+++ src/Game.jsx
@@ -3,21 +3,19 @@ import Board from "./Board";
 import { useState } from "react";
 
 function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
   const currentSquares = history[currentMove];
+  const xIsNext = currentMove % 2 === 0;
 
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
-    setHistory([...history, nextSquares]);
+    setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
-    setXIsNext(!xIsNext);
   }
 
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
-    setXIsNext(nextMove % 2 === 0);
   }
 
   const moves = history.map((squares, move) => {
